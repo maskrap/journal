0fc20264bc809fa6c457170923565a8d305a74d8
Sam Park and Tony Fabery <sam@>
2016-08-01 11:31:38 -0700

    Add prototypes

---
+++ js/journal.js
@@ -0,0 +1,41 @@
+function Journal(title, body) {
+  this.title = title;
+  this.body = body;
+}
+
+Journal.prototype.wordCount = function () {
+  var words = this.body.split();
+  return words.length;
+};
+
+Journal.prototype.letterCount = function (type) {
+  var words = this.body.toLowerCase.split();
+  var typeCount = [];
+  var letters = [];
+  words.forEach(word) {
+    letters.concat(word.split(''));
+  }
+  letters.forEach(letter) {
+    if (type = 'vowels' && letter === ('a' || 'e' || 'i' || 'o' || 'u')) {
+      typeCount.push(letter);
+    }
+    else {
+      typeCount.push(letter);
+    }
+  }
+  return typeCount.length;
+};
+
+Journal.prototype.getTeaser = function () {
+  var firstEight = [];
+  var words = this.body.split();
+  if (words.length <= 8)  {
+    firstEight = words;
+  }
+  else {
+    for (var i = 0; i <= 8; i++) {
+      firstEight.push(words[i]);
+    }
+  }
+  return firstEight.join(" ");
+};
